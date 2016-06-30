034ce8a4184d116cd066915beedc8c8a1d620919
Duncan <DuncN@users.noreply.github.com>
2016-06-30 23:03:33 +0100

    Adding signUpTest

---
+++ src/automatedChecks/signUpTest.java
@@ -0,0 +1,34 @@
+package automatedChecks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class signUpTest {
+	
+	@Test
+	public void main() {
+        WebDriver driver = new FirefoxDriver();
+
+        // Open website
+        driver.get("https://google-gruyere.appspot.com/358859087229/newaccount.gtl");
+
+        // Find user name field & enter name
+        driver.findElement(By.name("uid")).sendKeys("duncs5");
+        
+        // Find password field & enter password
+        driver.findElement(By.name("pw")).sendKeys("password1");
+        
+        // Find create account button & click it
+        driver.findElement(By.cssSelector("input[type='submit'][value='Create account']")).click();
+        
+        // Check that user is logged in
+       WebElement success = driver.findElement(By.className("message"));
+        String string = success.getText();
+        System.out.println(string);
+        Assert.assertTrue(string.contains("Account created."));
+   }
+}
